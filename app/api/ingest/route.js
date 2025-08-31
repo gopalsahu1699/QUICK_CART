0ad68dc270c32e058e3a195c00edced09aa2a8ff
Gopal krishn sahu <145195848+gopalsahu1699@users.noreply.github.com>
2025-08-31 09:30:05 +0530

    Fix: update inngest imports and config

--- app/api/ingest/route.js
+++ app/api/ingest/route.js
@@ -1,5 +1,5 @@
-import { serve } from "inngest/serve";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdatation } from "../../config/inngest";
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdatation } from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
